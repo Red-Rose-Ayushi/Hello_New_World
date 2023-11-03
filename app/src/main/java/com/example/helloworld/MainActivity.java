90343f77f9904ac7464a75ff7057cb69efe7ec35
Eve <eve.leduc2009@gmail.com>
2023-11-03 13:30:11 +0100

    Commit 1er essai

--- app/src/main/java/com/example/helloworld/MainActivity.java
+++ app/src/main/java/com/example/helloworld/MainActivity.java
@@ -1,6 +1,9 @@
 package com.example.helloworld;
 
 import androidx.appcompat.app.AppCompatActivity;
+import android.content.Intent;
+import android.view.View;
+import android.widget.Button;
 
 import android.os.Bundle;
 
@@ -8,9 +11,23 @@ import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
 
+    private Button btnjouer;
+    private Button btnparam;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        btnjouer= (Button) findViewById(R.id.btnjouer); // On associe l'objet bntjouer au xml btnjouer
+        btnjouer.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                onpenActivityJouer();
+            }
+        });
+    }
+    public void onpenActivityJouer(){
+        Intent intent = new Intent(this, ActivityJouer.class);
+        startActivity(intent);
     }
 }
